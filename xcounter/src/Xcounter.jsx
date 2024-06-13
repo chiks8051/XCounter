62d373b25e4b9e11d58d19a03b2fa127faf07ecd
Kumar Harshvardhan <sharshu1999hraj@gmail.com>
2024-06-13 08:29:58 +0530

    setting up the code

---
+++ xcounter/src/Xcounter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Xcounter = () => {
+  return (
+    <>
+      <h1>Counter App</h1>
+      <p>Count: </p>
+      <button>Increment</button>
+      <button>Decrement </button>
+    </>
+  );
+};
+
+export default Xcounter;
